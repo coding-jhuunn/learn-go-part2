e4ef35eeb25f53886dc08e337404aa4d9c87c76f
coding-jhuunn <codingatjhunn@gmail.com>
2024-09-18 15:05:14 +0800

    setup repo for go backend

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/coding-jhuunn/learn-go-part2
 
 go 1.22.4
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("hello world")
+
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+
+	if portString==""{
+		log.Fatal("Port is not found in env")
+	}
+	
+	router :=chi.NewRouter();
+
+	srv :=&http.Server{
+		Handler:router,
+		Addr: ":" +portString,
+	}
+
+	err := srv.ListenAndServe()
+
+	if err!=nil{
+		log.Fatal(err)
+	}
+	fmt.Println("Port", portString)
+
+	
 }
