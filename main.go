8d04c3a7b39cfd20e0c4d8cadad39b97187071b1
coding-jhuunn <codingatjhunn@gmail.com>
2024-09-18 15:41:01 +0800

    cors applied

--- main.go
+++ main.go
@@ -7,12 +7,12 @@ import (
 	"os"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("hello world")
-
+	
 	godotenv.Load(".env")
 
 	portString := os.Getenv("PORT")
@@ -23,13 +23,22 @@ func main() {
 	
 	router :=chi.NewRouter();
 
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: false,
+		MaxAge: 	  300,
+	}))		
 	srv :=&http.Server{
 		Handler:router,
 		Addr: ":" +portString,
 	}
 
+	log.Printf("server starting on port %v",portString)
 	err := srv.ListenAndServe()
-
 	if err!=nil{
 		log.Fatal(err)
 	}
